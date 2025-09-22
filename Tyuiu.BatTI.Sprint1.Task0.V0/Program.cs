809608941041626fcfab779c61c4a0243fd16ca0
Trofim Bat <Tyuiu.BatTI@outlook.com>
2025-09-22 18:44:08 +0500

    Add project files.

---
+++ Tyuiu.BatTI.Sprint1.Task0.V0/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.BatTI.Sprint1.Task0.V0.Lib;
+
+namespace Tyuiu.BatTI.Sprint1.Task0.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #0 | Выполнил: Бат Т. И. | ПИНб-25-1";
+            // длина строки 75 символов
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Базовые навыки работы в С#                                        *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #0                                                              *");
+            Console.WriteLine("* Выполнил: Бат Трофим Иванович | ПИНб-25-1                               *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая вычисляет выражение 10 / (2 + 3)            *");
+            Console.WriteLine("* и печатает результат на экране.                                         *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* 10 / (2 + 3)                                                            *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ                                                               *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(ds.Calculate());
+            Console.ReadLine();
+        }
+    }
+}
