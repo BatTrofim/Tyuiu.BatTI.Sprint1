c7a047aaa3479c61b72c3a4868d4504396dde121
Trofim Bat <Tyuiu.BatTI@outlook.com>
2025-09-30 18:46:27 +0500

    Task2

---
+++ Tyuiu.BatTI.Sprint1.Task2.V8.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.BatTI.Sprint1.Task2.V8.Lib;
+
+
+namespace Tyuiu.BatTI.Sprint1.Task1.V21.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            int a = 2;
+            int b = 1;
+            var res = ds.CalculatePerimetr(a, b);
+            Assert.AreEqual(6, res);
+        }
+    }
+}
--- Tyuiu.BatTI.Sprint1.Task2.V8/Program.cs
+++ Tyuiu.BatTI.Sprint1.Task2.V8/Program.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Tyuiu.BatTI.Sprint1.Task1.V21.Lib;
+using Tyuiu.BatTI.Sprint1.Task2.V8.Lib;
 
-namespace Tyuiu.BatTI.Sprint1.Task1.V21
+namespace Tyuiu.BatTI.Sprint1.Task2.V8
 {
     internal class Program
     {
@@ -16,31 +16,31 @@ namespace Tyuiu.BatTI.Sprint1.Task1.V21
             Console.WriteLine("****************************************************************************");
             Console.WriteLine("* Спринт #1                                                                *");
             Console.WriteLine("* Тема: Базовые навыки работы в С#                                         *");
-            Console.WriteLine("* Задание #1                                                               *");
-            Console.WriteLine("* Вариант #21                                                              *");
+            Console.WriteLine("* Задание #2                                                               *");
+            Console.WriteLine("* Вариант #8                                                               *");
             Console.WriteLine("* Выполнил: Бат Трофим Иванович | ПИНб-25-1                                *");
             Console.WriteLine("****************************************************************************");
             Console.WriteLine("* УСЛОВИЕ:                                                                 *");
             Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные,  *");
-            Console.WriteLine("* вычисляет результат по формуле (x * y) / (3 + y) и печатает его на экране.*");
+            Console.WriteLine("* выполняет указанные расчёты и печатает результат на экране.              *");
             Console.WriteLine("*                                                                          *");
             Console.WriteLine("****************************************************************************");
             Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                         *");
             Console.WriteLine("****************************************************************************");
 
-            double x, y;
+            int a, b;
 
-            Console.WriteLine("Введите значение x: ");
-            x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите длину первой стороны прямоугольника a: ");
+            a = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Введите значение y: ");
-            y = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите длину второй стороны прямоугольника b: ");
+            b = Convert.ToInt32(Console.ReadLine());
 
             Console.WriteLine("****************************************************************************");
-            Console.WriteLine("* РЕЗУЛЬТАТ:                                                               *");
+            Console.WriteLine("* РЕЗУЛЬТАТ: Периметр прямоульгоника                                       *");
             Console.WriteLine("****************************************************************************");
 
-            Console.WriteLine(ds.Calculate(x, y));
+            Console.WriteLine(ds.CalculatePerimetr(a, b));
 
             Console.ReadLine();
 
