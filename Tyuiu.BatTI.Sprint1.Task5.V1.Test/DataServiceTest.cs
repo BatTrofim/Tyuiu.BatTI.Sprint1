1d30aa37b3069c5947a1f2dc3b203fe69b4e1787
Trofim Bat <Tyuiu.BatTI@outlook.com>
2025-10-04 20:45:06 +0500

    Task5

---
+++ Tyuiu.BatTI.Sprint1.Task5.V1.Test/DataServiceTest.cs
@@ -0,0 +1,25 @@
+﻿using Tyuiu.BatTI.Sprint1.Task5.V1.Lib;
+
+namespace Tyuiu.BatTI.Sprint1.Task5.V1.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            double x1 = -1.0;
+            double y1 = 2.0;
+            double x2 = 3;
+            double y2 = 2.0;
+            DataService ds = new DataService();
+
+            double res = ds.DistanceBetweenDots(x1, y1, x2, y2);
+
+            int result = Convert.ToInt32(res);
+
+            int wait = 4;
+            Assert.AreEqual(wait, result);
+        }
+    }
+}
---
+++ Tyuiu.BatTI.Sprint1.Task5.V1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.BatTI.Sprint1.Task5.V1.Lib;
+
+namespace Tyuiu.BatTI.Sprint1.Task5.V1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1 | Выполнил: Бат Т.И. | ПИНб-25-1";
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* Спринт #1                                                                *");
+            Console.WriteLine("* Тема: Базовые навыки работы в С#                                         *");
+            Console.WriteLine("* Задание #5                                                               *");
+            Console.WriteLine("* Вариант #1                                                               *");
+            Console.WriteLine("* Выполнил: Бат Трофим Иванович | ПИНб-25-1                                *");
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                 *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные,  *");
+            Console.WriteLine("* выполняет указанные расчёты и печатает результат на экране.              *");
+            Console.WriteLine("*                                                                          *");
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                         *");
+            Console.WriteLine("****************************************************************************");
+
+            Console.WriteLine("Введите значение х:");
+            double x1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите значение у:");
+            double y1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите значение х:");
+            double x2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите значение у:");
+            double y2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                               *");
+            Console.WriteLine("****************************************************************************");
+
+            int res = Convert.ToInt32(ds.DistanceBetweenDots(x1, y1, x2, y2));
+            Console.WriteLine(res);
+            Console.ReadKey();
+        }
+    }
+}
