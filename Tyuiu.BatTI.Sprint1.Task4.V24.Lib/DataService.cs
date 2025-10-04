3f2e955818766ef27ca8ad46aa3c42e23ae4f119
Trofim Bat <Tyuiu.BatTI@outlook.com>
2025-10-04 19:56:19 +0500

    Task4

---
+++ Tyuiu.BatTI.Sprint1.Task4.V24.Lib/DataService.cs
@@ -0,0 +1,12 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+    
+namespace Tyuiu.BatTI.Sprint1.Task4.V24.Lib
+{
+    public class DataService : ISprint1Task4V24
+    {
+        public double Calculate(double x, double y)
+        {
+            return Math.Round((Math.Log(x * y)) / (x + (Math.Sqrt(2 * Math.Pow(y, 2)))), 3);
+        }
+    }
+}
---
+++ Tyuiu.BatTI.Sprint1.Task4.V24.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.BatTI.Sprint1.Task4.V24.Lib;
+
+namespace Tyuiu.BatTI.Sprint1.Task4.V24.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 1.0;
+            double y = 1.0;
+            double wait = 0;
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
