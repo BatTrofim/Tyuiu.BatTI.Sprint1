d96fea3d500285f7a5049ad74f1d00a169dc47ac
Trofim Bat <Tyuiu.BatTI@outlook.com>
2025-10-05 13:22:04 +0500

    Task6

---
+++ Tyuiu.BatTI.Sprint1.Task6.V14.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json.Linq;
+using Tyuiu.BatTI.Sprint1.Task6.V14.Lib;
+
+namespace Tyuiu.BatTI.Sprint1.Task6.V14.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidString()
+        {
+            string strTest = "Hello, World!";
+            DataService ds = new DataService();
+            bool res = ds.CheckLowerCaseRusLetters(strTest);
+            bool wait = false;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.BatTI.Sprint1.Task6.V14/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.BatTI.Sprint1.Task6.V14.Lib;
+
+namespace Tyuiu.BatTI.Sprint1.Task6.V14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1 | Выполнил: Бат Т.И. | ПИНб-25-1";
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* Спринт #1                                                                *");
+            Console.WriteLine("* Тема: Базовые навыки работы в С#                                         *");
+            Console.WriteLine("* Задание #6                                                               *");
+            Console.WriteLine("* Вариант #14                                                              *");
+            Console.WriteLine("* Выполнил: Бат Трофим Иванович | ПИНб-25-1                                *");
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                 *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные,  *");
+            Console.WriteLine("* выполняет указанные расчёты и печатает результат на экране.              *");
+            Console.WriteLine("*                                                                          *");
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                         *");
+            Console.WriteLine("****************************************************************************");
+
+            Console.WriteLine("Введите строку:");
+            string str = Console.ReadLine();
+
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                               *");
+            Console.WriteLine("****************************************************************************");
+
+            Console.WriteLine(ds.CheckLowerCaseRusLetters(str));
+            Console.ReadKey();
+        }
+    }
+}
