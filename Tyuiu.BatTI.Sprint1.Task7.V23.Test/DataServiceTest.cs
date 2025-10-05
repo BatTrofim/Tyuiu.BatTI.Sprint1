d6e2f8246237ee9f699f449fe78e1032351156c5
Trofim Bat <Tyuiu.BatTI@outlook.com>
2025-10-05 14:40:27 +0500

    Task7

---
+++ Tyuiu.BatTI.Sprint1.Task7.V23.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.BatTI.Sprint1.Task7.V23.Lib;
+
+namespace Tyuiu.BatTI.Sprint1.Task7.V23.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 1;
+            double y = 2;
+            double wait = 1.265;
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.BatTI.Sprint1.Task7.V23/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.BatTI.Sprint1.Task7.V23.Lib;
+
+namespace Tyuiu.BatTI.Sprint1.Task7.V23
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1 | Выполнил: Бат Т.И. | ПИНб-25-1";
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* Спринт #1                                                                *");
+            Console.WriteLine("* Тема: Базовые навыки работы в С#                                         *");
+            Console.WriteLine("* Задание #7                                                               *");
+            Console.WriteLine("* Вариант #23                                                              *");
+            Console.WriteLine("* Выполнил: Бат Трофим Иванович | ПИНб-25-1                                *");
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                 *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные,  *");
+            Console.WriteLine("* выполняет указанные расчёты и печатает результат на экране.              *");
+            Console.WriteLine("*                                                                          *");
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                         *");
+            Console.WriteLine("****************************************************************************");
+
+            Console.WriteLine("x - 10^sinx + (20 * x^2 / 3 * x^3) + cos(x^2 - y)                           ");
+
+            double x, y;
+            Console.WriteLine("Введите значение Х:");
+            x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите значение У:");
+            y = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                               *");
+            Console.WriteLine("****************************************************************************");
+
+            Console.WriteLine(ds.Calculate(x, y));
+            Console.ReadKey();
+        }
+    }
+}
